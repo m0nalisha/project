b0295c51993802837eee5b6cf8c2e7a9d060c631
m0nalisha <monalisha3011@gmail.com>
2020-04-13 14:47:15 +0530

    fixes

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -6,6 +6,8 @@ import { Injectable } from "@angular/core";
 export class AuthGuard implements CanActivate{
 
     constructor(private authservice:AuthService){}
+    path: ActivatedRouteSnapshot[];
+    route: ActivatedRouteSnapshot;
 
     canActivate(route:ActivatedRouteSnapshot,state:RouterStateSnapshot){
         return this.authservice.isAuthenticated()
